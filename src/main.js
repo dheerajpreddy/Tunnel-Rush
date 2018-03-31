e58c16c972a038b5e4bd976824a8702353552c75
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-03-31 23:20:00 +0530

    Obstacles work

--- src/main.js
+++ src/main.js
@@ -33,14 +33,14 @@ function main() {
   `;
   const fsSource2 = `
   varying lowp vec4 vColor;
-  precision lowp float;
+  precision mediump float;
   void main(void) {
   	float gray = dot(vColor.rgb, vec3(0.299, 0.587, 0.114));
   	gl_FragColor = vec4(vec3(gray), 1.0);
   }
   `;
   buffers = [];
-  tunnelObj = tunnelConstructor(1000);
+  tunnelObj = tunnelConstructor(100);
   obsObj = obsConstructor(10);
   buffers.push(initBuffers(gl, tunnelObj));
   buffers.push(initBuffers(gl, obsObj));
@@ -200,4 +200,55 @@ function loadShader(gl, type, source) {
     return null;
   }
   return shader;
+}
+
+
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn of mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
 }
--- src/obstacle.js
+++ src/obstacle.js
@@ -2,31 +2,31 @@ function obsConstructor(n) {
   obj = {};
   obj.num = n;
   var positions = [];
-  for (var i = 0; i < obj.n; i++) {
+  for (var i = 0; i < obj.num; i++) {
     if (i % 2 == 0) {
       positions.push(
-          1.4,  0.0,  0.5-2*i,
-          1.0,  1.0,  0.5-2*i,
-         -1.4,  0.0,  0.5-2*i,
-         -1.0, -1.0,  0.5-2*i,
+          1.4,  0.0,  0.5-20*i,
+          1.0,  1.0,  0.5-20*i,
+         -1.4,  0.0,  0.5-20*i,
+         -1.0, -1.0,  0.5-20*i,
         )
     }
     else if (i % 2 == 1) {
       positions.push(
-         -1.4,  0.0,  0.5-2*i,
-         -1.0, -1.0,  0.5-2*i,
-          1.0, -1.0,  0.5-2*i,
-          1.4,  0.0,  0.5-2*i,
+         -1.4,  0.0,  0.5-20*i,
+         -1.0, -1.0,  0.5-20*i,
+          1.0, -1.0,  0.5-20*i,
+          1.4,  0.0,  0.5-20*i,
         )
     }
   }
   var c = [1.0, 0.0, 0.0, 0.7];
   var colors = [];
-  for (var i = 0; i < obj.n; i++) {
+  for (var i = 0; i < obj.num; i++) {
     colors = colors.concat(c, c, c, c);
   }
   var indices = [];
-  for (var i = 0; i < obj.n; i++) {
+  for (var i = 0; i < obj.num; i++) {
     indices.push(4*i, 4*i + 1, 4*i + 2, 4*i, 4*i + 2, 4*i + 3);
   }
   obj.positions = positions;
